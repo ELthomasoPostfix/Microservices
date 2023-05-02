cda899778d1999cea6c34363219544c0d2edd466
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-05-03 00:52:47 +0200

    Propagate exceptions up to allow Flask error handlers.

--- shared/config.py
+++ shared/config.py
@@ -2,6 +2,7 @@ config = dict()
 
 # Public config
 config['POSTGRES_USER']='postgres'
+config['PROPAGATE_EXCEPTIONS'] = True   # Allow use of Flask app error handling features
 
 # Secret config
 config['POSTGRES_PASSWORD']='postgres'
