5365e1f71a19e2a7d4e6111f855a88544a5d92b1
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-05-02 22:19:46 +0200

    Drop account.id
    Simplify the account table to only
     a username and a password. Make
     the username the primary key.

--- accounts_persistence/init.sh
+++ accounts_persistence/init.sh
@@ -7,8 +7,7 @@ EOSQL
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "accounts" <<-EOSQL
     CREATE TABLE account(
-        id INTEGER PRIMARY KEY,
-        username TEXT NOT NULL,
+        username TEXT PRIMARY KEY,
         password TEXT NOT NULL
     );
 EOSQL
