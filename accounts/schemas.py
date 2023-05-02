7fc6c246d56614b09a61cafc5f4de17494e9fcaf
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-05-02 22:21:30 +0200

    Extend + document for account API.

--- accounts/schemas.py
+++ accounts/schemas.py
@@ -1,18 +1,31 @@
-from marshmallow import fields
+from marshmallow import Schema, fields
 
 from shared.schemas import MicroservicesResponseSchema, MicroservicesResultSchema
 
 
-class AccountSchema(MicroservicesResponseSchema):
+class AccountSchema(Schema):
+    """The primary account information"""
     id = fields.Integer(required=True, metadata={
         'description': 'The unique identifier of the user',
-    }, strict=True)
+    })
     username = fields.String(required=True, metadata={
         'description': 'The username of the user',
-    }, strict=True)
+    })
+
+
+class AccountResponseSchema(MicroservicesResponseSchema, AccountSchema):
+    """The output format of the Account resource endpoint"""
+    pass
 
 
 class AccountsSchema(MicroservicesResultSchema):
     result = fields.List(fields.Nested(AccountSchema), required=True, default=[], metadata={
         'description': 'A list of Movie resources',
+    })
+
+
+class RegisterBodySchema(Schema):
+    """The form body of an account registration"""
+    password = fields.String(required=True, location='form', metadata={
+        'description': 'The password to register the account with'
     })
