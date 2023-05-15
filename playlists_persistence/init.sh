e1319ad060f6a6d56961aea39f52ae3294f17592
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-05-15 22:22:04 +0200

    Add initial playlist microservice content.

---
+++ playlists_persistence/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE playlists;
+
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "playlists" <<-EOSQL
+    CREATE TABLE playlist (
+        id SERIAL PRIMARY KEY,
+        owner_username TEXT NOT NULL,
+        title TEXT NOT NULL,
+        UNIQUE (owner_username, title)
+    );
+
+    CREATE TABLE playlist_song (
+        playlist_id SERIAL,
+        song_artist TEXT NOT NULL,
+        song_title TEXT NOT NULL,
+        FOREIGN KEY (playlist_id) REFERENCES playlist(id),
+        UNIQUE (playlist_id, song_artist, song_title)
+    );
+EOSQL
