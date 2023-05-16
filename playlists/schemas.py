4c4a9f2334735e7934c4283f3eb9bdd0da341dac
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-05-16 02:28:23 +0200

    Add prelim schemas for playlist microservice.
    * Add PlaylistSongSchema to represent a song
     part of a playlist, for playlist fetch req
    * Add PlaylistSongBodySchema to represent
     a song to add to a playlist
    * Add PlaylistSchema to represent a playlist
     part of a response
    * Add PlaylistsResponseSchema to represent the
     full fetch of all playlists of a user
    * Add PlaylistResponseSchema to represent the
     full response of a singular playlist fetch

--- playlists/schemas.py
+++ playlists/schemas.py
@@ -1,3 +1,53 @@
+from copy import deepcopy
 from marshmallow import Schema, fields
 
 from shared.schemas import MicroservicesResponseSchema, MicroservicesResultSchema
+
+
+class PlaylistSongSchema(Schema):
+    """A song resource's information, as stored by a playlist"""
+    artist = fields.String(required=True, metadata={
+        'description': 'The artist of a song part of a playlist',
+    })
+    title = fields.String(required=True, metadata={
+        'description': 'The title of a song part of a playlist',
+    })
+
+
+class PlaylistSongBodySchema(Schema):
+    """A song resource's information, as stored by a playlist,
+    provided in a form body to be added to a playlist
+    """
+    artist = fields.String(required=True, location='form', metadata={
+        'description': 'The artist of a song to add to a playlist',
+    })
+    song_title = fields.String(required=True, location='form', metadata={
+        'description': 'The title of a song to add to a playlist',
+    })
+
+
+class PlaylistSchema(Schema):
+    """The Playlist resource's playlist specific information"""
+    id = fields.Integer(required=True, metadata={
+        'description': 'The unique identifier of the playlist',
+    })
+    owner = fields.String(required=True, metadata={
+        'description': 'The username of the user that owns the playlist',
+    })
+    title = fields.String(required=True, metadata={
+        'description': 'The user-designated title of the playlist',
+    })
+
+
+class PlaylistsResponseSchema(MicroservicesResultSchema):
+    """The output format of the Playlist resource endpoint"""
+    result = fields.List(fields.Nested(PlaylistSchema), required=True, default=[], metadata={
+        'description': 'The list of playlists of a user',
+    })
+
+
+class PlaylistResponseSchema(MicroservicesResultSchema, PlaylistSchema):
+    """The output format of the Playlist resource endpoint"""
+    result = fields.List(fields.Nested(PlaylistSongSchema), required=True, default=[], metadata={
+        'description': 'The list of songs part of the playlist',
+    })
