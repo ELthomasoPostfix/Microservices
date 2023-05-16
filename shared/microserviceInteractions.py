19909c70cf1f5d2b83f63fb8f8906eed167a9640
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-05-16 16:55:18 +0200

    Return the response from convenience functions.
    Instead of always returning None,
     return the requests.Response if success.

--- shared/microserviceInteractions.py
+++ shared/microserviceInteractions.py
@@ -1,8 +1,11 @@
 import requests
 
+from typing import Union
+
 from shared.exceptions import MicroserviceConnectionError, DoesNotExist
 
-def require_user_exists(username: str) -> None:
+
+def require_user_exists(username: str) -> Union[requests.Response, None]:
     """Require that the specified user exists according to
     the accounts microservice.
 
@@ -10,16 +13,19 @@ def require_user_exists(username: str) -> None:
     not return the expected, positive response.
 
     :param username: The username of the user to check existence of
+    :return: The microservice response if no exception
     """
     try:
         response = requests.get(f"http://accounts:5000/accounts/{username}")
         if response.status_code != 200:
             raise DoesNotExist(f"the user '{username}' does not exist")
+
+        return response
     # Explicitly set output values, to ensure graceful failure is handled appropriately
     except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
         raise MicroserviceConnectionError("could not reach the accounts microservice")
 
-def require_song_exists(artist: str, title: str) -> None:
+def require_song_exists(artist: str, title: str) -> Union[requests.Response, None]:
     """Require that the specified song exists according to
     the songs microservice.
 
@@ -28,16 +34,20 @@ def require_song_exists(artist: str, title: str) -> None:
 
     :param artist: The artist of the song to check existence of
     :param title: The title of the song to check existence of
+    :return: The microservice response if no exception
     """
     try:
         response = requests.get(f"http://songs:5000/songs/exist?artist={artist}&title={title}")
         if response.status_code != 200 or not response.json():
             raise DoesNotExist(f"the song with artist '{artist}' and title '{title}' does not exist")
+
+        return response
     # Explicitly set output values, to ensure graceful failure is handled appropriately
     except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
         raise MicroserviceConnectionError("could not reach the songs microservice")
 
-def require_playlist_exists(playlist_id: int) -> None:
+
+def require_playlist_exists(playlist_id: int) -> Union[requests.Response, None]:
     """Require that the specified playlist exists according to
     the playlists microservice.
 
@@ -45,11 +55,14 @@ def require_playlist_exists(playlist_id: int) -> None:
     not return the expected, positive response.
 
     :param playlist_id: The unique identifier of the playlist to check existence of
+    :return: The microservice response if no exception
     """
     try:
         response = requests.get(f"http://playlists:5000/playlists/{playlist_id}")
         if response.status_code != 200:
             raise DoesNotExist(f"the playlist with id '{playlist_id}' does not exist")
+
+        return response
     # Explicitly set output values, to ensure graceful failure is handled appropriately
     except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
         raise MicroserviceConnectionError("could not reach the playlists microservice")
