90cd46864691417597612506a913a4e22007233f
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-05-23 16:29:46 +0200

    Add WIP activity feed implementation.
    * Add playlist creation and song addition
     to a playlist to the activity feed
    * Add a /feeds/<username> API to represent
     the feed of a user

--- activity_feed/app.py
+++ activity_feed/app.py
@@ -1,10 +1,14 @@
+import requests
+
+from typing import List, Tuple
+from datetime import datetime
 from flask_apispec import MethodResource, doc, use_kwargs
 
 from shared.utils import initialize_micro_service, marshal_with_flask_enforced
 from shared.microserviceInteractions import require_user_exists
 from shared.exceptions import DoesNotExist, MicroserviceConnectionError, get_409_already_exists, get_404_does_not_exist, get_500_database_error, get_502_bad_gateway_error
 from shared.APIResponses import GenericResponseMessages as E_MSG, make_response_error, make_response_message
-from schemas import MicroservicesResponseSchema
+from schemas import MicroservicesResponseSchema, ActivityFeedResponseSchema, ActivityFeedBodySchema
 
 
 MICROSERVICE_NAME = "activity_feed"
@@ -17,7 +21,119 @@ APISPEC_CONFIG = {
 }
 app, api, docs, conn = initialize_micro_service(MICROSERVICE_NAME, DB_HOST, APISPEC_CONFIG)
 
+class ActivityFeed(MethodResource):
+    """The api endpoint that represents a single activity feed resource.
 
+    This resource supports the following project requirements
+        9. fetching a user's activity feed
+    """
+    @staticmethod
+    def route() -> str:
+        """Get the route to the Account resource.
+
+        :return: The route string
+        """
+        return "/feeds/<string:username>"
+
+    @doc(description='Get a single ActivityFeed resource, which represents a feed of the N most recent activities of all friends of the specified user\'s account.', params={
+        'username': {'description': 'The username of the chosen account'}
+    })
+    @use_kwargs(ActivityFeedBodySchema, location='query')
+    @marshal_with_flask_enforced(ActivityFeedResponseSchema, code=200)
+    def get(self, username: str, **kwargs):
+        """The query endpoint of the activity feed of a specific account.
+
+        :return: The activity feed
+        """
+        amount = int(kwargs["amount"])
+
+        require_user_exists(username)
+
+        # (date, title, desciption)
+        activity_feed: List[Tuple[datetime, str, str]] = []
+        def filtered_activity_feed():
+            return sorted(activity_feed, key=lambda activity: activity[0])[:amount]
+
+        # Fetch all friends of the user for which to construct the feed
+        friends_names: list = []  # A list of all the user's friends
+        try:
+            response = requests.get(f"http://friends:5000/friends/{username}?amount={amount}")
+            if response.status_code == 200:
+                friends_names = [
+                    friend_name
+                    for friend_info in response.json().get("result", list())
+                    if (friend_name := friend_info.get("friend_name", None)) is not None
+                ]
+
+        # Explicitly set output values, to ensure graceful failure is handled appropriately
+        except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
+            friends_names = []
+
+        # Call playlists API
+        playlists: list = []
+        for friend_name in friends_names:
+
+            # Fetch friend's playlists
+            try:
+                response = requests.get(f"http://playlists:5000/playlists/{friend_name}?amount{amount}")
+                if response.status_code == 200:
+                    for playlist_id in response.json().get("result", list()):
+
+                        # Skip malformed
+                        if "id" not in playlist_id or "title" not in playlist_id:
+                            continue
+
+                        playlists.append((playlist_id["id"], playlist_id["title"]))
+                        activity_feed.append((
+                            playlist_id["created"],
+                            f"Playlist created",
+                            f"{friend_name} created a playlist called {playlist_id['title']}")
+                        )
+
+                    activity_feed = filtered_activity_feed()
+
+            # Explicitly set output values, to ensure graceful failure is handled appropriately
+            except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
+                playlists = []
+
+            # Update the intermediary activity feed with song additions to playlists
+            for playlist_id, playlist_title in playlists:
+                # Fetch friend's playlist's songs
+                try:
+                    response = requests.get(f"http://playlists:5000/playlists/{playlist_id}?amount={amount}")
+                    if response.status_code == 200:
+                        activity_feed.extend([
+                            (
+                                playlist_song["created"],
+                                f"Song added to playlist",
+                                f"{friend_name} added the song {playlist_song['title']} by {playlist_song['artist']} to their playlist called {playlist_title}"
+                            )
+                            for playlist_song in response.json().get("result", list())
+                            if "artist" in playlist_song and "title" in playlist_song
+                        ])
+                        activity_feed = filtered_activity_feed()
+
+                # Explicitly set output values, to ensure graceful failure is handled appropriately
+                except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
+                    playlists = []
+
+            # Call playlists sharing API
+            # TODO
+            # TODO
+            # TODO
+            # TODO
+
+        # Format results for output
+        res = [
+            {
+                "date": date,
+                "description": description,
+                "title": title,
+            }
+            for date, title, description in activity_feed
+        ]
+
+        return make_response_message(E_MSG.SUCCESS, 200, result=res)
 
 
 @app.errorhandler(DoesNotExist)
@@ -34,3 +150,10 @@ def handle_db_connection_error(e):
     container being down
     """
     return get_502_bad_gateway_error(e, append_error=True)
+
+
+# Add resources
+api.add_resource(ActivityFeed, ActivityFeed.route())
+
+# Register apispec docs
+docs.register(ActivityFeed)
--- activity_feed/schemas.py
+++ activity_feed/schemas.py
@@ -2,3 +2,27 @@ from marshmallow import Schema, fields
 
 from shared.schemas import MicroservicesResponseSchema, MicroservicesResultSchema
 
+
+class ActivityFeedBodySchema(Schema):
+    amount = fields.String(required=True, location='query', metadata={
+        'description': 'The amount of activities to include in the output',
+    })
+
+
+class ActivitySchema(Schema):
+    title = fields.String(required=True, metadata={
+        'description': 'The title of the activity; a short name for the type of the activity',
+    })
+    description = fields.String(required=True, metadata={
+        'description': 'The description of the activity; a brief description of the specific contents of the activity',
+    })
+    date = fields.String(required=True, metadata={
+        'description': 'The date of the activity; the datetime when the activity took place',
+    })
+
+
+class ActivityFeedResponseSchema(MicroservicesResultSchema):
+    """The output format of the ActivityFeed resource endpoint"""
+    result = fields.List(fields.Nested(ActivitySchema), required=True, default=[], metadata={
+        'description': 'The list of N activities of a specified user\'s friends',
+    })
